19cd5759c0ab4e0e18e281e08c51cc4e5bf37ca0
naomichoy <41099656+naomichoy@users.noreply.github.com>
2023-11-13 23:40:29 +0000

    mahalanobis distance

--- cvpr_visualsearch.m
+++ cvpr_visualsearch.m
@@ -31,7 +31,7 @@ DESCRIPTOR_FOLDER = strcat(windows, 'descriptors');
 %% and within that folder, another folder to hold the descriptors
 %% we are interested in working with
 choices=["globalRGBhisto", "spatialGridColour", "EOH", "gridPlusEoh", "LBP"];
-choice_num = 5;
+choice_num = 1;
 DESCRIPTOR_SUBFOLDER=choices{choice_num};
 
 
@@ -96,12 +96,12 @@ for i=1:NIMG
     query=ALLFEAT(queryimg,:);
     
     % change visual search distance method here
-    thedst=cvpr_compare(query,candidate);
+%     thedst=cvpr_compare(query,candidate);
 %     thedst = l1_norm(query, candidate);
 %     thedst = cosineSim(candidate, query);
 
 %     thedst=mahalanobisDist(candidate, query, E);
-%     thedst=Eigen_Mahalanobis(candidate,E);
+    thedst=Eigen_Mahalanobis(candidate,E);
 
     dst=[dst ; [thedst i]];
 
